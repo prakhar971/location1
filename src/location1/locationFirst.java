e000ad2263a132ea04ddd09962f9b3170c4d0c17
prakhar <prakharshrivastava23399@gmail.com>
2022-05-08 15:57:11 +0530

    coomit this code

---
+++ src/location1/locationFirst.java
@@ -0,0 +1,11 @@
+package location1;
+
+public class locationFirst {
+public static void main(String[] args) {
+ String s = "prakhar";
+ 
+ for(int i = s.length()-1;i>=0;i--) {
+	 System.out.print(s.charAt(i));
+ }
+}
+}
